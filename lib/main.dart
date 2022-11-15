52795674cac94874497010416134863344538158
Wasim Zaman <wasimxaman13@gmail.com>
2022-11-15 12:25:42 +0500

    main file added

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,25 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Shop Application",
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("let's create a shop application"),
+    );
+  }
+}
